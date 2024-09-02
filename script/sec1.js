811aced8c3ec9bee8a3125065a8c3a77a76654d1
jinii9 <jinii915@naver.com>
2024-09-02 20:39:08 +0900

    feat: section1, section5 반응형

--- script/sec1.js
+++ script/sec1.js
@@ -11,4 +11,26 @@ var mySwiper = new Swiper(".sec1-swiper", {
     el: ".swiper-pagination", // 페이지 표시기
     clickable: true, // 페이지 번호 클릭 가능하게 설정
   },
+  breakpoints: {
+    500: {
+      slidesPerView: 1.5,
+      spaceBetween: 18,
+    },
+    768: {
+      slidesPerView: 3.5,
+      spaceBetween: 18,
+    },
+    922: {
+      slidesPerView: 3.5,
+      spaceBetween: 18,
+    },
+    1200: {
+      slidesPerView: 4.5,
+      spaceBetween: 18,
+    },
+    1500: {
+      slidesPerView: 5.5,
+      spaceBetween: 18,
+    },
+  },
 });
